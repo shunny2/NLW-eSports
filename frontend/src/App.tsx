a977318f0000cc6fc762ada2fe588f9c02a69429
Alexander Davis <alexander.davis.098@gmail.com>
2022-09-17 19:00:34 -0300

    update: Fixing new slider in project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,15 @@ import { useEffect, useState } from 'react';
 import { GameBanner } from './components/GameBanner';
 import { CreateAdBanner } from './components/CreateAdBanner';
 import { CreateAdModal } from './components/CreateAdModal';
+import { KeenSliderOptions, useKeenSlider } from 'keen-slider/react';
+import { CaretLeft, CaretRight } from 'phosphor-react';
+
+import "keen-slider/keen-slider.min.css";
 import './styles/main.css';
+
+import * as Dialog from '@radix-ui/react-dialog';
 import logo from './assets/logo-nlw-esports.svg';
 import axios from 'axios';
-import * as Dialog from '@radix-ui/react-dialog';
 
 interface Game {
   id: string;
@@ -19,10 +24,26 @@ interface Game {
 function App() {
   const [games, setGames] = useState<Game[]>([]);
 
+  const [currentSlide, setCurrentSlide] = useState<number>(0);
+  const [sildeOptions, setSlideOptions] = useState<KeenSliderOptions>({});
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>(sildeOptions);
+
   useEffect(() => {
     axios('http://localhost:3000/games')
       .then(response => {
         setGames(response.data);
+      })
+      .then(() => {
+        setSlideOptions({
+          initial: 0,
+          slideChanged: (slider) => {
+            setCurrentSlide(slider.track.details.rel);
+          },
+          slides: {
+            perView: 5,
+            spacing: 16,
+          }
+        })
       })
   }, [])
 
@@ -34,7 +55,7 @@ function App() {
         Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.
       </h1>
 
-      <div className='grid grid-cols-6 gap-6 mt-16'>
+      {/* <div className='grid grid-cols-6 gap-6 mt-16'>
         {games.map((game) => {
           return (
             <GameBanner
@@ -45,6 +66,36 @@ function App() {
             />
           )
         })}
+      </div> */}
+
+      <div ref={sliderRef} className='relative mt-16 rounded-lg keen-slider'>
+        <button
+          disabled={currentSlide === 0}
+          className='absolute rounded-full p-2 text-zinc-400 z-[1] left-2 top-1/2 -translate-y-1/2 bg-black/25 hover:bg-black/50 backdrop-blur disabled:opacity-0'
+          onClick={() => instanceRef.current?.prev()}
+        >
+          <CaretLeft className='w-8 h-8' />
+        </button>
+
+        {games.map((game) => {
+          return (
+            <GameBanner
+              key={game.id}
+              title={game.title}
+              bannerUrl={game.bannerUrl}
+              adsCount={game._count.ads}
+            />
+          )
+        })}
+
+        <button
+          disabled={currentSlide === games.length - 5}
+          className='absolute rounded-full p-2 text-zinc-400 z-[1] right-2 top-1/2 -translate-y-1/2 bg-black/25 hover:bg-black/50 backdrop-blur disabled:opacity-0'
+          onClick={() => instanceRef.current?.next()}
+        >
+          <CaretRight className='w-8 h-8' />
+        </button>
+
       </div>
 
       <Dialog.Root>
@@ -55,4 +106,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
--- frontend/src/components/GameBanner.tsx
+++ frontend/src/components/GameBanner.tsx
@@ -6,10 +6,10 @@ interface GameBannerProps {
 
 export function GameBanner(props: GameBannerProps) {
     return (
-        <a href="" className='relative rounded-lg overflow-hidden'>
-            <img src={props.bannerUrl} alt="game" />
+        <a href="#" className='relative rounded-lg overflow-hidden keen-slider__slide'>
+            <img className='w-full' src={props.bannerUrl} alt="game" />
 
-            <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <div className='pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
                 <strong className='font-bold text-white block'>{props.title}</strong>
                 <span className='text-zinc-300 text-sm block'>{props.adsCount} anúncio(s)</span>
             </div>
